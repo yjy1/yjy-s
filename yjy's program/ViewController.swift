e9cca30785be4321bcc1bbd76d7d29b21bcf5c24
explore <explore@exploredemac.local>
2018-03-05 22:28:55 +0800

    第二次修改 在ViewController.swift 中添加一行代码 print("the second modify!")

--- yjy's program/ViewController.swift
+++ yjy's program/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("the second modify!")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
