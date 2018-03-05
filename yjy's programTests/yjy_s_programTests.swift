a89c03059ca554b16d400f842091f763777022f8
explore <explore@exploredemac.local>
2018-03-05 22:06:00 +0800

    Initial Commit

---
+++ yjy's programTests/yjy_s_programTests.swift
@@ -0,0 +1,36 @@
+//
+//  yjy_s_programTests.swift
+//  yjy's programTests
+//
+//  Created by explore on 2018/3/5.
+//  Copyright © 2018年 explore. All rights reserved.
+//
+
+import XCTest
+@testable import yjy_s_program
+
+class yjy_s_programTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
